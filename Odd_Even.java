289afae592d9a846534f041d72c80c9c6228aea3
naveen1616071 <51507459+naveen1616071@users.noreply.github.com>
2019-07-20 14:42:25 +0530

    Add files via upload

---
+++ Odd_Even.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.Scanner;
+public class Odd_Even {
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int n=s.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+		s.close();
+	}
+}
