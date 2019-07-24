5e244a5b5aaf68333c3c8120daf0e0bcf1d5ed1d
naveen1616071 <51507459+naveen1616071@users.noreply.github.com>
2019-07-24 11:49:19 +0530

    Add files via upload

---
+++ time_complex.java
@@ -0,0 +1,34 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class time_complex {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int n=s.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		for(int k=0;k<n;k++)
+		{
+			for(int j=k+1;j<n;j++)
+			{
+				if(arr[k]>arr[j])
+				{
+					int temp=arr[j];
+					arr[j]=arr[k];
+					arr[k]=temp;
+				}
+			}
+		}
+		for(int l:arr)
+		{
+			System.out.print(l+" ");
+		}
+		s.close();
+	}
+
+}
